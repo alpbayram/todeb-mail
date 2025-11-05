b086bf6b40c77d35f985701e15cf6d0e61388808
Alp Eren Bayram <57071283+alpbayram@users.noreply.github.com>
2025-11-05 10:36:06 +0300

    Refactor email sending function in index.js

--- index.js
+++ index.js
@@ -68,8 +68,7 @@ module.exports = async (context) => {
 </div>
 
 `;
-        const html2 = `<!doctype html>
-
+        const html2 = `
     <div
       style='background-color:#ffffff;color:#405464;font-family:Avenir, "Avenir Next LT Pro", Montserrat, Corbel, "URW Gothic", source-sans-pro, sans-serif;font-size:16px;font-weight:400;letter-spacing:0.15008px;line-height:1.5;margin:0;padding:32px 0;min-height:100%;width:100%'
     >
@@ -181,7 +180,6 @@ module.exports = async (context) => {
         </tbody>
       </table>
     </div>
-
 `;
 
 
@@ -203,6 +201,7 @@ module.exports = async (context) => {
         return res.json({ ok: false, error: err.message }, 500);
     }
 };
+
 
 
 
